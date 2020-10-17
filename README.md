2aada5f08565f3ea62de9e9573d9851086a29d0b
Kolsyra <kolsyra@Kolsyra.local>
2020-10-17 13:36:22 +0200

    kmom05

---
+++ README.md
@@ -0,0 +1,1 @@
+chatserver for me.jsram.me
---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.info("User connected");
+    socket.on('send message', body => {
+        io.emit('message', body);
+    });
+});
+
+server.listen(3000);
